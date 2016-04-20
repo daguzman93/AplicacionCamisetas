fbe330ffeed2d35903567c705b41d103ae27aa54
daguzman93 <danielagustinas@gmail.com>
2016-04-20 21:36:17 +0200

    Proyecto inicial

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
+        <link type="text/css" rel="stylesheet" href="css/estilo.css"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>MAS DE MIL</title>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    </head>
+    <body>
+        <header>
+            <?php include("./cabecera.html"); ?>   
+        </header>
+        <main>
+
+        </main>
+        <footer class="page-footer">
+            <?php include("./piedepagina.html"); ?> 
+        </footer>
+
+    </body>
+</html>
