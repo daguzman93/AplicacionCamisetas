59b1cc0808ff825061ddc3fe286441a05ea56d22
daguzman93 <danielagustinas@gmail.com>
2016-04-21 16:31:55 +0200

    Insercion de los dropdown

--- index.php
+++ index.php
@@ -12,8 +12,9 @@ and open the template in the editor.
         <link type="text/css" rel="stylesheet" href="css/estilo.css"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>MAS DE MIL</title>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
         <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
+
     </head>
     <body>
         <header>
